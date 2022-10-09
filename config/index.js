9a1b0f1fcf164665cad77dd20aea349de2593f72
hzbnb <hzbnb@nb.com>
2022-10-09 15:54:01 +0800

    test

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  appid: "wx71941f6106ae1072",
+  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
+  template_id: "DdS5GVUoPVVdEPTkv4blqG62strhziW4kJfKRZ_dQPA",
+  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+};
--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("测试成功");
+const { appid, appsecret, template_id, touser } = require("./config/index");
+const axios = require("axios");
+let token =
+  "61_SL296k8YSRmKaJRmc9E6Q4jyjK0xVokAiKadZXH4wGLFEGl8hOm1iB3SqV1_HqUaAMh2KjKKB_71rsZhuYuuISa2HcJb0d1Cp-gY3yptlWauhT5EZaMiO5HIjDqFGVafBKm6Mgx-aHpNlHcbOBRiAAADQG";
+
+function get_token() {
+  axios
+    .get(
+      `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`
+    )
+    .then((res) => {
+      console.log(res.data);
+      token = res.data.access_token;
+      send();
+    });
+}
+
+function send() {
+  axios
+    .post(
+      `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${token}`,
+      {
+        touser,
+        template_id,
+        topcolor: "#FF0000",
+        data: {
+          ce: {
+            value: "黄先生",
+            color: "#173177",
+          },
+        },
+      }
+    )
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+get_token();
