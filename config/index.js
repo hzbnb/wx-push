4edea589e8ec561796ba4435cb148f1270ea1c49
hzbnb <hzbnb@nb.com>
2022-11-27 17:50:07 +0800

    11-27

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   appid: "wx71941f6106ae1072",
   appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "DdS5GVUoPVVdEPTkv4blqG62strhziW4kJfKRZ_dQPA",
+  template_id: "fzGx9ko6oYDEOsOu8to7Ya4PLmpF_17d4B9qeAiiLQ0",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 };
--- index.js
+++ index.js
@@ -1,50 +1,60 @@
 const { appid, appsecret, template_id, touser } = require("./config/index");
 const axios = require("axios");
-let token =
-  "61_SL296k8YSRmKaJRmc9E6Q4jyjK0xVokAiKadZXH4wGLFEGl8hOm1iB3SqV1_HqUaAMh2KjKKB_71rsZhuYuuISa2HcJb0d1Cp-gY3yptlWauhT5EZaMiO5HIjDqFGVafBKm6Mgx-aHpNlHcbOBRiAAADQG";
-
-function get_token() {
-  axios
-    .get(
-      `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`
-    )
-    .then((res) => {
-      console.log(res.data);
-      token = res.data.access_token;
-      send();
-    });
+const token = "";
+const sendData = {
+  touser,
+  template_id,
+  topcolor: "#FF0000",
+  data: {
+    nowDate: {
+      value: "黄先生",
+      color: "#173177",
+    },
+    weather: {
+      value: "黄先生",
+      color: "#173177",
+    },
+    city: {
+      value: "黄先生",
+      color: "#173177",
+    },
+    low: {
+      value: "黄先生",
+      color: "#173177",
+    },
+    high: {
+      value: "黄先生",
+      color: "#173177",
+    },
+    loveDate: {
+      value: "黄先生",
+      color: "#173177",
+    },
+    txt: {
+      value: "黄先生",
+      color: "#173177",
+    },
+  },
+};
+async function get_token() {
+  const {
+    data: { access_token },
+  } = await axios.get(
+    `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`
+  );
+  return access_token;
+}
+async function send(token) {
+  const res = await axios.post(
+    `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${token}`,
+    sendData
+  );
+  console.log(res.data);
 }
 
-function send() {
-  axios
-    .post(
-      `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${token}`,
-      {
-        touser,
-        template_id,
-        topcolor: "#FF0000",
-        data: {
-          ce: {
-            value: "黄先生",
-            color: "#173177",
-          },
-        },
-      }
-    )
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+async function run() {
+  let token = await get_token();
+  send(token);
 }
 
-function git_ip() {
-  axios.get(`http://mip.chinaz.com/`).then((res) => {
-    console.log(res.data);
-  });
-}
-
-// get_token();
-
-git_ip();
+run();
