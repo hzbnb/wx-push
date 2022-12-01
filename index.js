9cb03bd11ac57f57c10219e82919d9f1ee97e161
hzbnb <hzbnb@nb.com>
2022-12-01 18:11:08 +0800

    push7

--- index.js
+++ index.js
@@ -9,13 +9,8 @@ const sendData = {
 async function run() {
   const res = await axios({
     method: "get",
-    url: "https://api.mcloc.cn/love/?type=string",
-    headers: {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    },
+    url: "https://api.mcloc.cn/love/",
   });
-
   console.log(res.data);
 
   // let token = await get_token();
