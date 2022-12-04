81c7659f1ee2592928288ed0cbd99eb741f95f09
hzbnb~ <61189185+hzbnb@users.noreply.github.com>
2022-12-04 15:27:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx71941f6106ae1072",
   appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "TUgz8f5zkdTYWmwyTC_akIPhbEUyzMiNc0O2JicK8Rk",
+  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
