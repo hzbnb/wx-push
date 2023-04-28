53d59e0b552757a7d5de8f04e9cf5f551c5f41a1
wu0536 <132089896+wu0536@users.noreply.github.com>
2023-04-29 00:58:34 +0800

    Create index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx71941f6106ae1072",
   appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
   template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "okJ1l6Iw_MrZRsNWOfGynhi6DvAY",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "邢台",
